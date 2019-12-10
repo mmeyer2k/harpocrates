fcb0d1a7155c8d577c5939b7d93c70788ee00222
mmeyer2k <m.meyer2k@gmail.com>
2019-12-10 13:26:51 -0600

    no message

---
+++ HarpocratesTests/EngineTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Harpocrates.Tests
+{
+    [TestClass()]
+    public class EngineTests
+    {
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            string a = "AAAA";
+            string b = Harpocrates.Engine.Encrypt(a, "BBBB");
+            string c = Harpocrates.Engine.Decrypt(b, "BBBB");
+
+            Assert.AreEqual(a, c);
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ HarpocratesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HarpocratesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HarpocratesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0ee33aa-bacc-4199-afdf-ffb148a6b070")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
